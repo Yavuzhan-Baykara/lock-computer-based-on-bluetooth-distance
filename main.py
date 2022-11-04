fbde5acaeb5ef7828492232a08c990de10772ab7
Yavzan Baggins <yavuzhan96@gmail.com>
2022-11-04 15:45:38 +0300

    main

---
+++ main.py
@@ -0,0 +1,38 @@
+import bluetooth
+import ctypes
+import subprocess
+from subprocess import Popen, PIPE
+import time
+import multiprocessing
+from multiprocessing import Pool
+
+def discover_devices():
+    nearby_devices = bluetooth.discover_devices()
+    return nearby_devices
+    
+
+def activate():
+    target_name = "My Phone"
+    target_address = "7c:fd:6b:79:da:56"    
+    pool = Pool(20)
+    multiple_results = [pool.apply_async(discover_devices,) for i in range(10)]
+
+    for bdaddr in multiple_results[-1].get():
+        if target_name == bluetooth.lookup_name( bdaddr ):
+            target_address = bdaddr
+            break
+    values = bluetooth.find_service(address=target_address)
+    pool.close()
+    return values
+
+if __name__ == "__main__":
+    while True:
+
+        if not activate() == []:
+            print("Bluetooth is active")
+
+        else:
+            print(activate())
+            cmd='rundll32.exe user32.dll, LockWorkStation'
+            subprocess.call(cmd)
+
